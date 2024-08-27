4c2e3cd61572edcfe18c28b6af997de96865c158
CHOR NARIN <narintkh178@gmail.com>
2024-08-27 12:08:35 +0700

    add new query method

--- src/main/java/chornarin/com/kh/Phone_Shop/Service/BrandService.java
+++ src/main/java/chornarin/com/kh/Phone_Shop/Service/BrandService.java
@@ -1,11 +1,17 @@
 package chornarin.com.kh.Phone_Shop.Service;
 
+import java.util.List;
+
 import chornarin.com.kh.Phone_Shop.models.Brand;
 
 public interface BrandService {
     Brand create(Brand brand);
     Brand GetbyId(Integer id);
     Brand Update(Integer id, Brand brand);
+    List<Brand> getsBrands();
+    List<Brand> getsBrands(String name);
+
+
 
     
 } 
